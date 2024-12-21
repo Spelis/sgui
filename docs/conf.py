215dbcd5d19ab685edcdfc3b08cc2411cb2e6874
Elis Eriksson <Spelis@duck.com>
2024-12-21 15:14:29 +0100

    im going insaneeeeeee dfjugh dfgdfjh

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
-import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+from pathlib import Path
+sys.path.insert(0, str(Path('..', 'src').resolve()))
 
 project = "SGUI"
 author = "Elis Eriksson"
