66243ac722db65a279d346a6ae7f2efe1db2295c
Elis Eriksson <Spelis@duck.com>
2024-12-21 15:21:15 +0100

    im so desperate

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,8 @@
 import sys
 from pathlib import Path
 sys.path.insert(0, str(Path('..', 'src').resolve()))
+sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.insert(0, str(Path(__file__).parent.parent / 'src'))
 
 project = "SGUI"
 author = "Elis Eriksson"
@@ -10,4 +12,11 @@ source_suffix = {
     '.rst': 'restructuredtext',
     '.txt': 'markdown',
     '.md': 'markdown',
+}
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
 }
