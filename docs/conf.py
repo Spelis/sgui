7c82955816df3718131bf01a0b3c0df87e41b9cc
Elis Eriksson <Spelis@duck.com>
2024-12-21 14:42:41 +0100

    omg i hate documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
 project = "SGUI"
 author = "Elis Eriksson"
 html_theme = "sphinx_rtd_theme"
+extensions = ['myst_parser']
 source_suffix = {
     '.rst': 'restructuredtext',
     '.txt': 'markdown',
