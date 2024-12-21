083ded9d741605a3b14b57fc9fa8a2bf8424903f
Elis Eriksson <Spelis@duck.com>
2024-12-21 15:03:18 +0100

    idk

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 project = "SGUI"
 author = "Elis Eriksson"
 html_theme = "sphinx_rtd_theme"
-extensions = ['myst_parser']
+extensions = ['myst_parser','sphinx.ext.autodoc']
 source_suffix = {
     '.rst': 'restructuredtext',
     '.txt': 'markdown',
